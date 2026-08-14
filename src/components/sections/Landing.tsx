c9f8747994439d67f1ea167b3763e4eaf3819322
Dylan H Lin <117963104+DeltA3V1@users.noreply.github.com>
2026-08-14 03:33:12 +0000

    Made the landing screen more responsive on mobile

--- src/components/sections/Landing.tsx
+++ src/components/sections/Landing.tsx
@@ -3,22 +3,28 @@ import scrollToStation from "../../lib/scrollToStation";
 
 export function Landing() {
   return (
-    <section id="landing" className="mt-39 gap-18 font-inter flex flex-col items-center justify-between bg-dominant transition-colors duration-300">
-      <div className="gap-24 flex flex-col items-center justify-center">
-        <div className="gap-22 flex flex-col items-center justify-center">
-          <h1 className="text-8xl">[hello]</h1>
-          <div className="gap-6 flex flex-col items-center justify-center">
-            <h2 className="text-5xl">I'm Dylan,</h2>
-            <p className="w-76 text-2xl/[1.4] text-center">an upcoming CS student @ the University of Washington.</p>
-          </div>
-          <div className="gap-4 w-full flex flex-col items-center justify-center">
-            <LandingButton bg="bg-accent" text="Portfolio" textColor="text-surface" onClick={() => scrollToStation("portfolio")} />
-            <LandingButton bg="bg-surface" text="Contact Me" textColor="text-accent" onClick={() => scrollToStation("contact")} />
-          </div>
+    <section 
+      id="landing" 
+      className="min-h-[100dvh] font-inter flex flex-col bg-dominant transition-colors duration-300"
+    >
+      <div className="flex-1 flex flex-col items-center justify-center gap-12 px-4">
+        <h1 className="text-8xl">[hello]</h1>
+        
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h2 className="text-4xl md:text-5xl">I'm Dylan,</h2>
+          <p className="max-w-xs text-lg md:text-2xl/[1.4] text-center px-4">
+            an upcoming CS student @ the University of Washington.
+          </p>
         </div>
-
+        
+        <div className="w-full max-w-xs flex flex-col items-center justify-center gap-4 mt-4">
+          <LandingButton bg="bg-accent" text="Portfolio" textColor="text-surface" onClick={() => scrollToStation("portfolio")} />
+          <LandingButton bg="bg-surface" text="Contact Me" textColor="text-accent" onClick={() => scrollToStation("contact")} />
+        </div>
       </div>
-      <p>end</p>
+
+      <div className="bg-secondary w-full py-8">
+      </div>
     </section>
   )
 }
