16cdc96862690afe60d893a74747aefd26573413
Dylan H Lin <117963104+DeltA3V1@users.noreply.github.com>
2026-08-21 01:56:37 +0000

    Fixed station node spacing

--- src/components/map/StationNode.tsx
+++ src/components/map/StationNode.tsx
@@ -5,7 +5,7 @@ export function StationNode({ station } : { station : Station }) {
   return (
     <button className="w-full flex flex-col py-2 items-end z-10" onClick={() => scrollToStation(station.id as StationId)}>
       <h3 className="text-secondary text-lg text-right mr-29">{station.label}</h3>
-      <div className="bg-dominant w-5 h-5 rounded-full border-2 mr-22.5"></div>
+      <div className="bg-dominant w-5 h-5 rounded-full border-2 mr-21"></div>
     </button>
   )
 }
